e640cc04c6804850445040d1bb05206a6ca574e4
MasatoMakino <unagiinu.g@gmail.com>
2019-07-26 16:37:51 +0900

    gulpfile内のパスを変数に格納。

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,25 @@
 const path = require("path");
 const { series, parallel, src, dest, watch } = require("gulp");
 
-const server = require("gulptask-dev-server")("./dist");
+const srcDir = "./src/";
+const distDir = "./dist/";
+
+const server = require("gulptask-dev-server")(distDir);
 exports.server = server;
 
-const ejsGlob = "./src/**/*.ejs";
-const ejs = require("gulptask-ejs")([ejsGlob, "!./src/**/_*.ejs"], "./dist");
+const ejsGlob = srcDir + "**/*.ejs";
+const ejs = require("gulptask-ejs")([ejsGlob, "!_*.ejs"], distDir);
 exports.ejs = ejs;
 
-const sassDir = path.resolve("./src/scss");
-const sass = require("gulptask-sass")(sassDir + "/**/style.scss", "./dist/css");
+const sassDir = path.resolve(srcDir, "scss");
+const sass = require("gulptask-sass")(
+  sassDir + "/**/style.scss",
+  path.resolve(distDir, "css")
+);
 exports.sass = sass;
 
-const imgDir = path.resolve("./src/img");
-const images = require("gulptask-imagemin")(imgDir, "./dist");
+const imgDir = path.resolve(srcDir, "img");
+const images = require("gulptask-imagemin")(imgDir, distDir);
 exports.images = images;
 
 const watchTasks = cb => {
