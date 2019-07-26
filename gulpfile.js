99ba151ba4a6505482c88bcb7438768e570b7d1c
MasatoMakino <unagiinu.g@gmail.com>
2019-07-26 16:18:46 +0900

    画像最適化処理をgulpタスクに置き換え。

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const { series, parallel, src, dest, watch } = require("gulp");
 
 const server = require("gulptask-dev-server")("./dist");
@@ -7,8 +8,13 @@ const ejsGlob = "./src/**/*.ejs";
 const ejs = require("gulptask-ejs")([ejsGlob, "!./src/**/_*.ejs"], "./dist");
 exports.ejs = ejs;
 
+const imgDir = path.resolve("./src/img");
+const images = require("gulptask-imagemin")(imgDir, "./dist");
+exports.images = images;
+
 const watchTasks = cb => {
   watch(ejsGlob, ejs);
+  watch(imgDir + "/**/*", images);
   cb();
 };
 exports.watchTasks = watchTasks;
