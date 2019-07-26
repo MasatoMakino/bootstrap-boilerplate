ac8618d99448fda8f3f7a65c03b07100c1effe47
MasatoMakino <unagiinu.g@gmail.com>
2019-07-26 15:13:59 +0900

    ejs処理をgulpタスクに入れ替え。

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,14 @@
+const { series, parallel, src, dest, watch } = require("gulp");
+
 const server = require("gulptask-dev-server")("./dist");
 exports.server = server;
+
+const ejsGlob = "./src/**/*.ejs";
+const ejs = require("gulptask-ejs")([ejsGlob, "!./src/**/_*.ejs"], "./dist");
+exports.ejs = ejs;
+
+const watchTasks = cb => {
+  watch(ejsGlob, ejs);
+  cb();
+};
+exports.watchTasks = watchTasks;
