6c86961d97bc9ebc3006e2c506268fbc3f54f3d1
MasatoMakino <unagiinu.g@gmail.com>
2020-09-21 16:33:37 +0900

    update : dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,22 @@ const { series, parallel, src, dest, watch } = require("gulp");
 const srcDir = "./src/";
 const distDir = "./dist/";
 
-const server = require("gulptask-dev-server").get(distDir);
+const server = require("gulptask-dev-server").generateTask(distDir);
 
 const ejsGlob = srcDir + "**/*.ejs";
-const ejs = require("gulptask-ejs").get([ejsGlob, "!./**/_*.ejs"], distDir);
+const ejs = require("gulptask-ejs").generateTask(
+  [ejsGlob, "!./**/_*.ejs"],
+  distDir
+);
 
 const sassDir = path.resolve(srcDir, "scss");
-const sass = require("gulptask-sass").get(
+const sass = require("gulptask-sass").generateTask(
   sassDir + "/**/style.scss",
   path.resolve(distDir, "css")
 );
 
 const imgDir = path.resolve(srcDir, "img");
-const images = require("gulptask-imagemin").get(imgDir, distDir);
+const images = require("gulptask-imagemin").generateTask(imgDir, distDir);
 
 const copyGlob = "./src/**/*.+(htm|html)";
 const copy = () => {
@@ -28,7 +31,9 @@ const {
   bundleDevelopment,
   bundleProduction,
   watchBundle,
-} = require("gulptask-webpack").get({ configPath: "./webpack.config.js" });
+} = require("gulptask-webpack").generateTasks({
+  configPath: "./webpack.config.js",
+});
 
 const clean = (cb) => {
   rimraf(distDir, cb);
