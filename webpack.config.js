c1989c23b67c6c208cb846d1d62ecd4cf41049ac
MasatoMakino <unagiinu.g@gmail.com>
2021-06-02 17:05:52 +0900

    change : migrate to bootstrap 5
    related : #4

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,15 @@
 "use strict";
-const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
   entry: {
-    main: "./src/js/main.js"
+    main: "./src/js/main.js",
   },
   output: {
     path: `${__dirname}/dist/js`,
-    filename: "[name].js"
+    filename: "[name].js",
   },
   resolve: {
-    extensions: [".js", ".webpack.js", ".web.js"]
+    extensions: [".js", ".webpack.js", ".web.js"],
   },
   module: {
     rules: [
@@ -19,22 +17,12 @@ module.exports = {
         test: /\.js$/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env"]
-        }
-      }
-    ]
+          presets: ["@babel/preset-env"],
+        },
+      },
+    ],
   },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery",
-      Popper: ["popper.js", "default"],
-      // In case you imported plugins individually, you must also require them here:
-      Util: "exports-loader?Util!bootstrap/js/dist/util",
-      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown"
-    })
-  ],
+  plugins: [],
   optimization: {
     splitChunks: {
       cacheGroups: {
@@ -42,9 +30,9 @@ module.exports = {
           chunks: "initial",
           name: "vendor",
           test: /node_modules/,
-          enforce: true
-        }
-      }
-    }
-  }
+          enforce: true,
+        },
+      },
+    },
+  },
 };
